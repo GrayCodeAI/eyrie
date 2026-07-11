6371c9fcaa138e0f32803da5c8ef6e9c28e67fc6
Lakshman Patel <Lakshmanp230@gmail.com>
2026-07-11 11:14:10 +0530

    fix(ci): gofumpt internal/grpc/server_grpc.go

--- internal/grpc/server_grpc.go
+++ internal/grpc/server_grpc.go
@@ -20,6 +20,7 @@ func (JSONCodec) Name() string { return "json" }
 func (JSONCodec) Marshal(v interface{}) ([]byte, error) {
 	return json.Marshal(v)
 }
+
 func (JSONCodec) Unmarshal(data []byte, v interface{}) error {
 	return json.Unmarshal(data, v)
 }
